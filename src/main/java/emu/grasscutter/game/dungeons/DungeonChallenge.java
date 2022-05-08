74c119ece7400548135fd940aac33689b1a50a40
Ming <flutterming@gmail.com>
2022-05-08 20:30:54 +0200

    1.修改副本掉落逻辑，单次掉落不超过5件物品，且掉落物品随机

--- src/main/java/emu/grasscutter/game/dungeons/DungeonChallenge.java
+++ src/main/java/emu/grasscutter/game/dungeons/DungeonChallenge.java
@@ -18,144 +18,156 @@ import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
 import it.unimi.dsi.fastutil.ints.IntSet;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class DungeonChallenge {
-	private final Scene scene;
-	private final SceneGroup group;
-	
-	private int challengeIndex;
-	private int challengeId;
-	private boolean success;
-	private boolean progress;
-	
-	private int score;
-	private int objective = 0;
-	private IntSet rewardedPlayers;
+    private final Scene scene;
+    private final SceneGroup group;
 
-	public DungeonChallenge(Scene scene, SceneGroup group) {
-		this.scene = scene;
-		this.group = group;
-		this.setRewardedPlayers(new IntOpenHashSet());
-	}
+    private int challengeIndex;
+    private int challengeId;
+    private boolean success;
+    private boolean progress;
 
-	public Scene getScene() {
-		return scene;
-	}
+    private int score;
+    private int objective = 0;
+    private IntSet rewardedPlayers;
 
-	public SceneGroup getGroup() {
-		return group;
-	}
-	
-	public int getChallengeIndex() {
-		return challengeIndex;
-	}
+    public DungeonChallenge(Scene scene, SceneGroup group) {
+        this.scene = scene;
+        this.group = group;
+        this.setRewardedPlayers(new IntOpenHashSet());
+    }
 
-	public void setChallengeIndex(int challengeIndex) {
-		this.challengeIndex = challengeIndex;
-	}
+    public Scene getScene() {
+        return scene;
+    }
 
-	public int getChallengeId() {
-		return challengeId;
-	}
+    public SceneGroup getGroup() {
+        return group;
+    }
 
-	public void setChallengeId(int challengeId) {
-		this.challengeId = challengeId;
-	}
+    public int getChallengeIndex() {
+        return challengeIndex;
+    }
 
-	public int getObjective() {
-		return objective;
-	}
+    public void setChallengeIndex(int challengeIndex) {
+        this.challengeIndex = challengeIndex;
+    }
 
-	public void setObjective(int objective) {
-		this.objective = objective;
-	}
+    public int getChallengeId() {
+        return challengeId;
+    }
 
-	public boolean isSuccess() {
-		return success;
-	}
+    public void setChallengeId(int challengeId) {
+        this.challengeId = challengeId;
+    }
 
-	public void setSuccess(boolean isSuccess) {
-		this.success = isSuccess;
-	}
-	
-	public boolean inProgress() {
-		return progress;
-	}
+    public int getObjective() {
+        return objective;
+    }
 
-	public int getScore() {
-		return score;
-	}
-	
-	public int getTimeLimit() {
-		return 600;
-	}
+    public void setObjective(int objective) {
+        this.objective = objective;
+    }
 
-	public IntSet getRewardedPlayers() {
-		return rewardedPlayers;
-	}
+    public boolean isSuccess() {
+        return success;
+    }
 
-	public void setRewardedPlayers(IntSet rewardedPlayers) {
-		this.rewardedPlayers = rewardedPlayers;
-	}
+    public void setSuccess(boolean isSuccess) {
+        this.success = isSuccess;
+    }
 
-	public void start() {
-		this.progress = true;
-		getScene().broadcastPacket(new PacketDungeonChallengeBeginNotify(this));
-	}
-	
-	public void finish() {
-		this.progress = false;
-		
-		getScene().broadcastPacket(new PacketDungeonChallengeFinishNotify(this));
-		
-		if (this.isSuccess()) {
-			// Call success script event
-			this.getScene().getScriptManager().callEvent(EventType.EVENT_CHALLENGE_SUCCESS, null);
-			
-			// Settle
-			settle();
-		} else {
-			this.getScene().getScriptManager().callEvent(EventType.EVENT_CHALLENGE_FAIL, null);
-		}
-	}
-	
-	private void settle() {
-		getScene().getDungeonSettleObservers().forEach(o -> o.onDungeonSettle(getScene()));
-		
-		getScene().getScriptManager().callEvent(EventType.EVENT_DUNGEON_SETTLE, new ScriptArgs(this.isSuccess() ? 1 : 0));
-	}
+    public boolean inProgress() {
+        return progress;
+    }
 
-	public void onMonsterDie(EntityMonster entity) {
-		score = getScore() + 1;
-		
-		getScene().broadcastPacket(new PacketChallengeDataNotify(this, 1, getScore()));
-		
-		if (getScore() >= getObjective() && this.progress) {
-			this.setSuccess(true);
-			finish();
-		}
-	}
-	
-	public void getStatueDrops(Player player) {
-		DungeonData dungeonData = getScene().getDungeonData();
-		if (!isSuccess() || dungeonData == null || dungeonData.getRewardPreview() == null || dungeonData.getRewardPreview().getPreviewItems().length == 0) {
-			return;
-		}
-		
-		// Already rewarded
-		if (getRewardedPlayers().contains(player.getUid())) {
-			return;
-		}
-		
-		List<GameItem> rewards = new ArrayList<>();
-		for (ItemParamData param : getScene().getDungeonData().getRewardPreview().getPreviewItems()) {
-			rewards.add(new GameItem(param.getId(), Math.max(param.getCount(), 1)));
-		}
-		
-		player.getInventory().addItems(rewards, ActionReason.DungeonStatueDrop);
-		player.sendPacket(new PacketGadgetAutoPickDropInfoNotify(rewards));
-		
-		getRewardedPlayers().add(player.getUid());
-	}
+    public int getScore() {
+        return score;
+    }
+
+    public int getTimeLimit() {
+        return 600;
+    }
+
+    public IntSet getRewardedPlayers() {
+        return rewardedPlayers;
+    }
+
+    public void setRewardedPlayers(IntSet rewardedPlayers) {
+        this.rewardedPlayers = rewardedPlayers;
+    }
+
+    public void start() {
+        this.progress = true;
+        getScene().broadcastPacket(new PacketDungeonChallengeBeginNotify(this));
+    }
+
+    public void finish() {
+        this.progress = false;
+
+        getScene().broadcastPacket(new PacketDungeonChallengeFinishNotify(this));
+
+        if (this.isSuccess()) {
+            // Call success script event
+            this.getScene().getScriptManager().callEvent(EventType.EVENT_CHALLENGE_SUCCESS, null);
+
+            // Settle
+            settle();
+        } else {
+            this.getScene().getScriptManager().callEvent(EventType.EVENT_CHALLENGE_FAIL, null);
+        }
+    }
+
+    private void settle() {
+        getScene().getDungeonSettleObservers().forEach(o -> o.onDungeonSettle(getScene()));
+
+        getScene().getScriptManager().callEvent(EventType.EVENT_DUNGEON_SETTLE, new ScriptArgs(this.isSuccess() ? 1 : 0));
+    }
+
+    public void onMonsterDie(EntityMonster entity) {
+        score = getScore() + 1;
+
+        getScene().broadcastPacket(new PacketChallengeDataNotify(this, 1, getScore()));
+
+        if (getScore() >= getObjective() && this.progress) {
+            this.setSuccess(true);
+            finish();
+        }
+    }
+
+    public void getStatueDrops(Player player) {
+        DungeonData dungeonData = getScene().getDungeonData();
+        if (!isSuccess() || dungeonData == null || dungeonData.getRewardPreview() == null || dungeonData.getRewardPreview().getPreviewItems().length == 0) {
+            return;
+        }
+
+        // Already rewarded
+        if (getRewardedPlayers().contains(player.getUid())) {
+            return;
+        }
+
+        List<GameItem> rewards = new ArrayList<>();
+        List<ItemParamData> previewItems = Arrays.asList(getScene().getDungeonData().getRewardPreview().getPreviewItems());
+        Collections.shuffle(previewItems);
+        int rewardsCount = 0;
+        for (ItemParamData param : previewItems) {
+            if (param.getId() == 102 || param.getId() == 105 || param.getId() == 202) {
+                rewards.add(new GameItem(param.getId(), Math.max(param.getCount(), 1)));
+            } else {
+                if (rewardsCount > 5) continue;
+                rewards.add(new GameItem(param.getId(), Math.max(param.getCount(), 1)));
+                rewardsCount++;
+            }
+
+        }
+
+        player.getInventory().addItems(rewards, ActionReason.DungeonStatueDrop);
+        player.sendPacket(new PacketGadgetAutoPickDropInfoNotify(rewards));
+
+        getRewardedPlayers().add(player.getUid());
+    }
 }
